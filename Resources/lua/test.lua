d7059662337e198ac65f1e434d29dfb2c1fa9289
DianChen <mirchd.nine@gmail.com>
2018-12-23 22:29:49 +0800

    add test lua

--- Resources/lua/test.lua
+++ Resources/lua/test.lua
@@ -642,4 +642,51 @@ GetArg(1, 2)
 -- print("counter = " .. mycounter())
 -- print("counter = " .. mycounter())
 
---------------------------closure-------------------------
+-- print(_VERSION)
+-- io.popen("luai.exe -v")
+--------------------------loop-------------------------
+-- 一个table
+local languages =
+{
+	"lua",
+	"c",
+	"c++",
+	"java",
+	"python",
+}
+local elementnum = #(languages)
+
+-- while 循环
+print("\nwhile loop result:")
+
+local index = 1
+while index <= elementnum do
+	print("[" .. index .. "] = " .. languages[index])
+	index = index + 1
+end
+
+-- repeat 循环
+print("\nrepeat loop result:")
+
+index = 1
+repeat
+	print("[" .. index .. "] = " .. languages[index])
+	index = index + 1
+until index > elementnum
+
+-- for 循环
+print("\nfor loop result:")
+
+for i = 1,elementnum,1 do
+	print("[" .. i .. "] = " .. languages[i])
+end
+
+-- generic for 循环
+print("\ngeneric for loop result:")
+for i,value in ipairs(languages) do
+	print("[" .. i .. "] = " .. value)
+end
+print('-----pairs------')
+for i,value in pairs(languages) do
+	print("[" .. i .. "] = " .. value)
+end
