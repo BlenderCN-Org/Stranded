1852d69b94d297c33ec6cc650d02f7cac524f52e
DianChen <mirchd.nine@gmail.com>
2018-12-23 13:17:54 +0800

    lua test file

--- Resources/lua/test.lua
+++ Resources/lua/test.lua
@@ -621,3 +621,25 @@ end
 GetArg(1, 2)
 
 --]]
+
+--------------------------closure-------------------------
+-- -- 计数器
+-- function create_a_counter()
+--     local count = 0
+--     return function()
+--         count = count + 1
+--         return count
+--     end
+-- end
+
+-- -- 调用部分
+-- local mycounter = create_a_counter()
+
+-- print("counter = " .. mycounter())
+-- print("counter = " .. mycounter())
+-- print("counter = " .. mycounter())
+-- print("counter = " .. mycounter())
+-- print("counter = " .. mycounter())
+-- print("counter = " .. mycounter())
+
+--------------------------closure-------------------------
